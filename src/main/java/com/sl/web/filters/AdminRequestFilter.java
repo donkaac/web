7baa5fbc7008ef071584ff0fc6f66f4fa047a6ca
Lakdimuthu <lakdimuthu@javainstitute.edu.lk>
2020-04-14 22:08:34 +0530

    local Commit

--- src/main/java/com/sl/web/filters/AdminRequestFilter.java
+++ src/main/java/com/sl/web/filters/AdminRequestFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "AdminRequestFilter", urlPatterns = "/admin/*")
+@WebFilter(filterName = "AdminRequestFilter", urlPatterns = "/web/*")
 public class AdminRequestFilter implements Filter {
     public void destroy() {
     }
